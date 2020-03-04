8bb659902278500ac82417a6715ecc83a125d1cc
dorian <dekolis1218@gmail.com>
2020-03-03 21:49:12 -0800

    Add code

--- ResistanceCalculatorImproved.py
+++ ResistanceCalculatorImproved.py
@@ -1,1 +1,5 @@
 resistances = []
+
+resistor_amount = int(input("How many resistors will you be adding? "))
+for resistor in range(resistor_amount):
+    resistances.append(float(input("What is the resistance of resistor " + str(resistor+1) + "? ")))
